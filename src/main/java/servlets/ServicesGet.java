194a63dd49111f75b2fd2c1919674c85faa112cc
emanuele <emanuele@mbp-di-rocco.station>
2017-11-26 23:36:02 +0100

    aggiunto servizio per i grafici

--- src/main/java/servlets/ServicesGet.java
+++ src/main/java/servlets/ServicesGet.java
@@ -6,24 +6,38 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import org.hibernate.Criteria;
-import org.hibernate.Query;
+ 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+
+import com.google.gson.Gson;
 
 import basic.HibernateUtil;
 import entities.Csv;
+import entities.ReleaseIt;
+import entities.User;
 
 @Path("/")
 public class ServicesGet {
 	
+	private Session session;
+
 	@GET
 	@Path("/csvForIdPolarion")
 	@Produces("application/json")
@@ -35,5 +49,44 @@ public class ServicesGet {
 		return Response.accepted().entity(csv).build();
 //		return csv;
 	}
+	
+	@GET
+	@Path("/releaseIT")
+	@Produces("application/json")
+	public Response getReleaseIt() {
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Query query = session.createQuery("SELECT COUNT(*),YEAR(data_creazione) FROM ReleaseIt GROUP BY YEAR(data_creazione)");
+		List<Gson> result = query.getResultList();
+
+		session.getTransaction().commit();
+		session.close();
+		
+		return Response.accepted().entity(result).build();
+
+	}
+	
+	@GET
+	@Path("/allReleaseIT")
+	@Produces("application/json")
+	public Response getAllReleaseIt() {
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Query query = session.createQuery("SELECT COUNT(*),YEAR(data_creazione) FROM ReleaseIt GROUP BY YEAR(data_creazione)");
+		List<Gson> result = query.getResultList();
+
+		session.getTransaction().commit();
+		session.close();
+		
+		return Response.accepted().entity(result).build();
+
+	}
+	
+	
+	
 
 }
